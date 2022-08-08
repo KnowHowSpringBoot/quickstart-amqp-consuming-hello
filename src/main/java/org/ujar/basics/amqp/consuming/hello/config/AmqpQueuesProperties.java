1a3596dda7d95be1194e29303a3468e6e368e885
Dima Denysenko <coding@dimdnk.com>
2022-08-08 19:08:47 +0300

    feat: use records for props

--- src/main/java/org/ujar/basics/amqp/consuming/hello/config/AmqpQueuesProperties.java
+++ src/main/java/org/ujar/basics/amqp/consuming/hello/config/AmqpQueuesProperties.java
@@ -1,20 +1,20 @@
 package org.ujar.basics.amqp.consuming.hello.config;
 
-import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.lang.NonNull;
 import org.springframework.validation.annotation.Validated;
 
-@Data
 @Validated
 @ConstructorBinding
 @ConfigurationProperties(prefix = "ujar.amqp.queues")
-public class AmqpQueuesProperties {
+public record AmqpQueuesProperties (@NonNull String greeterExchange, @NonNull String greeterQueue) {
 
-  @NonNull
-  private final String greeterExchange;
+  public String getGreeterExchange() {
+    return greeterExchange();
+  }
 
-  @NonNull
-  private final String greeterQueue;
+  public String getGreeterQueue() {
+    return greeterQueue();
+  }
 }
